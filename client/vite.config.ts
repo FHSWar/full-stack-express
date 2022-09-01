3c26a5f66cf09d8212f4cda47fd2be99fc46c657
FHSWar <fhswar0504@163.com>
2022-09-01 09:32:58 +0800

    feat: client做浏览器兼容，并支持jsx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
 export default defineConfig({
-    plugins: [vue()]
+    resolve: {
+        alias: {
+            /* https://antfu.me/posts/isomorphic-dirname
+            dirname(fileURLToPath(import.meta.url))是esmodule下__dirname的替代品 */
+            '@': resolve(dirname(fileURLToPath(import.meta.url)), './src/')
+        }
+    },
+    plugins: [
+        // 兼容上古浏览器
+        legacy({
+            targets: ['Chrome 70'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+        }),
+        // 支持sfc
+        vue(),
+        // 支持jsx
+        vueJsx()
+    ]
 })
